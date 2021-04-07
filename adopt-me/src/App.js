9759f80e9add7d7b0fa1e675de09d350361807a3
ysKim <znxkznxk1030@gmail.com>
2021-04-07 23:57:14 +0900

    add ThemeContext & Modal

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,25 +1,27 @@
-import React, { StrictMode } from "react";
+import React, { StrictMode, useState } from "react";
 import { render } from "react-dom";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
+import ThemeContext from "./ThemeContext";
 
 const App = () => {
+  const themeHook = useState("darkblue");
+
   return (
     <StrictMode>
-      <div>
-        <h1 id="something-important">Adopt Me!</h1>
-        <Router>
-          {/* <Link to="/">Adopt Me!</Link> */}
-          <Switch>
-            <Route exact path="/">
-              <SearchParams />
-            </Route>
-            <Route path="/details/:id" component={Details}>
-            </Route>
-          </Switch>
-        </Router>
-      </div>
+      <ThemeContext.Provider value={themeHook}>
+        <div>
+          <h1 id="something-important">Adopt Me!</h1>
+          <Router>
+            {/* <Link to="/">Adopt Me!</Link> */}
+            <Switch>
+              <Route exact path="/" component={SearchParams} />
+              <Route path="/details/:id" component={Details} />
+            </Switch>
+          </Router>
+        </div>
+      </ThemeContext.Provider>
     </StrictMode>
   );
   2;
--- adopt-me/src/Details.js
+++ adopt-me/src/Details.js
@@ -2,15 +2,19 @@ import React from "react";
 import pet from "@frontendmasters/pet";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundry";
+import ThemeContext from "./ThemeContext";
+import { useHistory } from "react-router-dom";
+import Modal from "./Modal";
 
 class Details extends React.Component {
-  state = { loading: true }
-  
+  state = { loading: true, showModal: false };
+
   componentDidMount() {
-    console.log(this.props)
-    throw new Error();
+    console.log(this.props);
+    // throw new Error();
     pet.animal(this.props.match.params.id).then(({ animal }) => {
       this.setState({
+        url: animal.url,
         name: animal.name,
         animal: animal.type,
         location: `${animal.contact.address.city},
@@ -23,25 +27,58 @@ class Details extends React.Component {
     }, console.error);
   }
 
-  
+  toggleModal = () => this.setState({ showModal: !this.state.showModal });
+  adopt = () => {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const history = useHistory();
+    history.push(this.state.url);
+  };
+
   render() {
     if (this.state.loading) {
-      return <h1>loading ...</h1>
+      return <h1>loading ...</h1>;
     }
 
-    const { animal, breed, location, description, name, media} = this.state;
-    
+    const {
+      animal,
+      breed,
+      location,
+      description,
+      name,
+      media,
+      showModal,
+      url,
+    } = this.state;
+
     return (
       <div className="details">
         <Carousel media={media} />
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} - ${breed} - ${location}`}</h2>
-          <button>Adopt {name}</button>
+          {/* <ThemeContext.Consumer> */}
+            {/* {(themeHook) => { */}
+              <button
+                style={{ backgroundColor: "blue" }}
+                onClick={this.toggleModal}
+              >
+                Adopt {name}
+              </button>;
+            {/* }} */}
+          {/* </ThemeContext.Consumer> */}
           <p>{description}</p>
+          {showModal ? (
+            <Modal>
+              <div>
+                <h1>Would you like to adopt {name}?</h1>
+                <button onClick={this.adopt}>Yes</button>
+                <button onClick={this.toggleModal}>No, I'm a monster</button>
+              </div>
+            </Modal>
+          ) : null}
         </div>
       </div>
-    )
+    );
   }
 }
 
@@ -50,5 +87,5 @@ export default function DetailsWithErrorBoundary(props) {
     <ErrorBoundary>
       <Details {...props} />
     </ErrorBoundary>
-  )
-};
+  );
+}

57fec2782d98d7bb592cefaa597b141cf6026ca7
ysKim <znxkznxk1030@gmail.com>
2021-06-17 20:38:40 +0530

    adjust prettier

--- ent-vote-app/src/App.js
+++ ent-vote-app/src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <header className="App-header"></header>
     </div>
   );
 }

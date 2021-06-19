6d8a928badc7f46659cb59e3531d6f18f433fc0d
ysKim <znxkznxk1030@gmail.com>
2021-06-19 17:22:57 +0900

    navbar init

--- ent-vote-app/src/Navbar/index.js
+++ ent-vote-app/src/Navbar/index.js
@@ -1,15 +1,36 @@
 import React from "react";
-import "./Navbar.css";
+import "./Navbar.scss";
 
 class Navbar extends React.Component {
-  render () {
+  render() {
     return (
-      <div className="navbar" >
-        <h1>navbar</h1>
-      </div>
-    )
-  }
+      <header>
+        <div class="container">
+          <h1 class="logo">ent<span>honor</span></h1>
+          <nav class="site-nav">
+            <ul>
+              <li>
+                <a href="">Home</a>
+              </li>
+              <li>
+                <a href="">Weekly</a>
+              </li>
+              <li>
+                <a href="">Programs</a>
+              </li>
+              <li>
+                <a href="">Talents</a>
+              </li>
+            </ul>
+          </nav>
 
+          <div class="menu-toggle">
+            <div class="hamburger"></div>
+          </div>
+        </div>
+      </header>
+    );
+  }
 }
 
 export default Navbar;
